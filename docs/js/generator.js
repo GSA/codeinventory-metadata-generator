3c40c1bcd6a222b394f5403ecdd35cd570584692
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-11-23 15:44:08 -0500

    update gh-pages with validation

--- docs/js/generator.js
+++ docs/js/generator.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  var schema = {};
+  var schemaLoaded = false;
+
   function parseForm() {
     var metadata = formToObject("form", { includeEmptyValuedElements: true });
     metadata.openSourceProject = parseInt(metadata.openSourceProject);
@@ -7,8 +10,31 @@ $(document).ready(function() {
     return metadata;
   }
 
+  function validate(metadata) {
+    var errorMapping = {
+      ".name": "Enter a project name.",
+      ".description": "Enter a description of the project.",
+      ".license": "The license should be a URL (or blank if there is no license URL).",
+      ".tags": "Enter at least one tag.",
+      ".contact.email": "Enter a valid contact email address."
+    };
+    var ajv = new Ajv({ allErrors: true });
+    var valid = ajv.validate(schema, metadata);
+    if (!valid) {
+      $("#validation li").remove();
+      for (var i = 0; i < ajv.errors.length; i++) {
+        var error = ajv.errors[i];
+        $("#validation ul").append("<li>" + errorMapping[error.dataPath] + "</li>");
+      }
+    }
+    $("#validation").toggle(!valid);
+  }
+
   function updateResult(event) {
     var metadata = parseForm();
+    if (schemaLoaded) {
+      validate(metadata);
+    }
     $("#json-result").val(JSON.stringify(metadata, null, 2));
     $("#yaml-result").val(YAML.dump(metadata));
   }
@@ -29,5 +55,10 @@ $(document).ready(function() {
     tags: true,
     width: null,
     tokenSeparators: [',', ' ']
+  });
+
+  $.getJSON("codeinventory-schema.json", function (data) {
+    schema = data;
+    schemaLoaded = true;
   });
 });
